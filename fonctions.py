9d3141e08c7ea38f6202cceab2c7f4037ad65047
Baptiste LEFEBVRE <baptiste.lefebvre@telecom-paris.fr>
2025-10-06 11:28:28 +0200

    Grid generation and a bit of testing

--- fonctions.py
+++ fonctions.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 # necessite scikit-image 
 from skimage import io as skio
+from skimage import color, filters
 import IPython
 from skimage.transform import rescale
 
@@ -19,15 +20,14 @@ def viewimage(im, normalize=True,z=1,order=0,titre='',displayfilename=False):
     else:
         imin=imin.clip(0,255)/255 
     imin=(imin*255).astype(np.uint8)
-    filename=tempfile.mktemp(titre+'.png')
-    if displayfilename:
-        print (filename)
-    plt.imsave(filename, imin, cmap='gray')
-    IPython.display.display(IPython.display.Image(filename))
-
+    plt.imshow(imin)
+    plt.axis('off')
+    plt.title(titre)
+    plt.show()
+    
 def derive(im, dir=1):
-    s=im.shape()
-    im2= np.zeros((s,s))
+    s=im.shape
+    im2= np.zeros(s)
     for i in range (s[0]):
         for j in range (s[1]):
             if dir==1:
@@ -45,3 +45,75 @@ def derive(im, dir=1):
                 else:
                     im2[i,j]=(im[i+1,j]-im[i-1,j])/2
     return im2
+
+'''
+def gradient(im):
+    lab_img = color.rgb2lab(im)  
+    viewimage(lab_img)
+    gradient = filters.sobel(color.rgb2gray(lab_img))  
+'''
+
+def square_grid(im, step=0):
+    w, h = im.shape
+    print(h, w)
+    if step == 0:
+        step = min(h, w) // 10
+    x = np.arange(0, h, step)
+    y = np.arange(0, w, step)
+    xv, yv = np.meshgrid(x, y, indexing='ij')
+    print(xv.shape, yv.shape)
+    return xv, yv
+
+def hexagonal_grid(im, step=0):
+    w, h = im.shape
+    print(h, w)
+    if step == 0:
+        step = min(h, w) // 5
+    x1 = np.arange(0, h, step*np.sqrt(3))
+    y1 = np.arange(0, w, step)
+    xv1, yv1 = np.meshgrid(x1, y1, indexing='ij')
+    print(xv1, yv1)
+    x2 = np.arange(step*np.sqrt(3)//2, h, step*np.sqrt(3))
+    y2 = np.arange(step//2, w, step)
+    xv2, yv2 = np.meshgrid(x2, y2, indexing='ij')
+
+    xv = np.concatenate([xv1.flatten(), xv2.flatten()])
+    yv = np.concatenate([yv1.flatten(), yv2.flatten()])
+    
+    return xv, yv
+
+def read_image(filename, grey):
+    im = skio.imread(filename, as_gray=grey)
+    return im
+
+def test(n):
+    if n == 0:
+        # Test square grid
+        im = read_image('Lake.png', True)
+        xv, yv = square_grid(im)
+        plt.plot(xv, yv, marker='o', color='k', linestyle='none')
+        plt.axis('scaled')
+        plt.show()
+    if n == 2: 
+        # Test hexagonal grid
+        im = read_image('Lake.png', True)
+        xv, yv = hexagonal_grid(im)
+        plt.plot(xv, yv, marker='o', color='k', linestyle='none')
+        plt.axis('scaled')
+        plt.show()
+    if n == 3:
+        # Test gradient
+        im = read_image('Lake.png', True)
+        grad = derive(im)
+        viewimage(grad)
+    if n == 4:
+        # Test view image
+        im = read_image('Lake.png', False)
+        viewimage(im)
+    if n == 5:
+        # Test gradient 2
+        im = read_image('Lake.png', True)
+        grad = gradient(im)
+        viewimage(grad)
+        
+test(5)
